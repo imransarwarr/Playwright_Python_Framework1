814fb9cf1f11347c14d42b1c056e8816ef646842
imransarwarr <imran.swe2@gmail.com>
2026-05-17 12:06:56 +0500

    conftest file is updated & basic testcase execution

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture(scope="session")
+def playwright_instance():
+    with sync_playwright() as p:
+        yield p
+
+
+@pytest.fixture(scope="session", params=["chromium", "firefox","webkit"])
+def browser(request, playwright_instance):
+    browser_name = request.param
+
+    browser = getattr(playwright_instance, browser_name).launch(
+        headless=False
+    )
+
+    yield browser
+
+    browser.close()
+
+
+@pytest.fixture()
+def page(browser):
+    page = browser.new_page()
+
+    yield page
+
+    page.close()
